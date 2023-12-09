55a53e10790bab1656dc23e99b535c6eaa063e25
Jorge Correia <14-10254@usb.ve>
2023-12-08 22:05:05 -0400

    segm

--- segmentation.py
+++ segmentation.py
@@ -2,21 +2,24 @@ import kmeans as km
 import img
 from PIL import Image
 
-df, ancho,altura = img.toRGB('descarga.jpeg')
+df, ancho,altura = img.toRGB('img03.jpeg')
 
-imgkm = km.kmeans(3,df.values)
+k = [2,4,8,16,32,64,128,256]
+iter = 200
+for i in k:
+    imgkm = km.kmeans(i,df.values,iter)
 
-clusters_img = imgkm.inicializarCentroides()
+    clusters_img = imgkm.inicializarCentroides()
 
-dfNew = df.copy()
+    dfNew = df.copy()
 
-for i in range(len(df)):
-    centroid_values = imgkm.centroides[clusters_img[i]]
-    dfNew.at[i, 'R'] = round(centroid_values[0])
-    dfNew.at[i, 'G'] = round(centroid_values[1])
-    dfNew.at[i, 'B'] = round(centroid_values[2])
+    for j in range(len(df)):
+        centroid_values = imgkm.centroides[clusters_img[j]]
+        dfNew.at[j, 'R'] = round(centroid_values[0])
+        dfNew.at[j, 'G'] = round(centroid_values[1])
+        dfNew.at[j, 'B'] = round(centroid_values[2])
 
-# Imprimir la iamgen final
-array = dfNew[['R','G','B']].values.reshape((altura,ancho,3)).astype('uint8')
-imgfinal = Image.fromarray(array)
-imgfinal.show()
+    # Imprimir la iamgen final
+    array = dfNew[['R','G','B']].values.reshape((altura,ancho,3)).astype('uint8')
+    imgfinal = Image.fromarray(array)
+    imgfinal.save('./clustered/'+'waifu'+'_k_'+str(i)+'.png')
