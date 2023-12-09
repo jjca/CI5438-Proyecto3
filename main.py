175ea6ff15a107f47e9a06d28ab46cfaa75f76ae
Jorge Correia <14-10254@usb.ve>
2023-12-08 21:30:19 -0400

    updated

--- main.py
+++ main.py
@@ -9,19 +9,17 @@ df = pd.read_csv('./iris.csv')
 #sns.FacetGrid(df,hue="species",height=3).map(sns.histplot,"petal_length",stat="density").add_legend()
 
 #sns.set_style("whitegrid")
-#sns.pairplot(df,hue="species",size=3)
-
-sns.FacetGrid(df,hue="species",height=6).map(plt.scatter_3d,"petal_length","petal_width").add_legend()
-
-#plt.scatter(iris_setosa['petal_length'], iris_setosa['petal_width'])
+sns.pairplot(df,hue="species",height=3)
 plt.show()
 
-""" df.drop(columns=['species'],axis = 1,inplace=True)
+perms = ['sepal_length','sepal_width','petal_width','petal_length']
+
+df.drop(columns=['species'],axis = 1,inplace=True)
 df.drop(columns=['sepal_length'],axis = 1, inplace=True)
 df.drop(columns=['sepal_width'],axis = 1, inplace=True)
-kmean = kmeans.kmeans(3 ,df.values)
+kmean = kmeans.kmeans(3,df.values)
 Y = kmean.inicializarCentroides()
 
 plt.scatter(df['petal_length'], df['petal_width'], c=Y)
-plt.scatter(kmean.centroides[:,0], kmean.centroides[:,1],c = range(len(kmean.centroides)),marker="*",s = 200)
-plt.show() """
+plt.scatter(kmean.centroides[:,0], kmean.centroides[:,1],c ='blue',marker="*",s = 200)
+plt.show()
--- segmentation.py
+++ segmentation.py
@@ -1,18 +1,22 @@
 import kmeans as km
 import img
 from PIL import Image
+
 df, ancho,altura = img.toRGB('descarga.jpeg')
 
 imgkm = km.kmeans(3,df.values)
-clusters_img = imgkm.inicializarCentroides()
-#print(clusters_img.tolist())
 
-print(imgkm.centroides)
+clusters_img = imgkm.inicializarCentroides()
+
+dfNew = df.copy()
+
+for i in range(len(df)):
+    centroid_values = imgkm.centroides[clusters_img[i]]
+    dfNew.at[i, 'R'] = round(centroid_values[0])
+    dfNew.at[i, 'G'] = round(centroid_values[1])
+    dfNew.at[i, 'B'] = round(centroid_values[2])
 
 # Imprimir la iamgen final
-array = df[['R','G','B']].values.reshape((altura,ancho,3)).astype('uint8')
+array = dfNew[['R','G','B']].values.reshape((altura,ancho,3)).astype('uint8')
 imgfinal = Image.fromarray(array)
 imgfinal.show()
-
-#plt.imshow(df.astype(np.uint8))
-#plt.show()
